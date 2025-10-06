11d5891f5c6f343fd845c361c86c74f912844eb2
Ana <anasiddiqi07@outlook.com>
2025-10-06 14:46:11 +0100

    edit

--- 01.drawing/sketch.js
+++ 01.drawing/sketch.js
@@ -1,9 +1,16 @@
 function setup() {
   createCanvas(400, 400);
+  background (0, 0, 200);
 }
 
 function draw() {
-  background(220);
-  circle(50, 50, 50)
-  circle(30, 50, 50)
+  //background(220);
+  circle(width/2, 50, 50);
+  fill(255, 0, 0)
+  circle(60, mouseX, 50);
+  fill(0, 255, 0)
+  circle(70, 50, 50);
+  fill(0, 0, 255)
+  circle(80, 50, 50);
+
 }
