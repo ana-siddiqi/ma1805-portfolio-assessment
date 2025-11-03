b46a60f85b40db200c5f70fbf492ca545ad75313
ana-siddiqi <anasiddiqi07@outlook.com>
2025-11-03 20:02:06 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,37 +1,48 @@
 let img;
-let hum;
+let sound;
+let video;
 let capture;
 
+
 function preload(){
-  img = loadImage('Antenna.jpg');
-  hum = loadSound("humm.wav");
-  video = createVideo("TheSea.mov");
+  img = loadImage('IMG_8431.JPG');
+  sound = loadSound("gentle-ocean-waves-3-300839.mp3");
+  video = createVideo("compressed.mp4");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   capture = createCapture(VIDEO);
-  capture.size(320,240)
+  capture.size(320, 240);
   capture.hide();
 
   video.hide();
-  video.play
+  video.loop();
+  video.volume(0);
+  video.play();
+ 
 }
 
 function draw() {
-  background(220);
+ background(0);
 image(img, 0, 100, img.width/2, img.height/2);
-image(capture,200,200,320,240);
-filter(INVERT);
-image(video, 300, 30, 240, 320);
+image(capture,50,300,320,240);
+image(video, 400, 50, 320, 240);
+
+fill(255);
+textSize(32);
+textAlign(CENTER, CENTER);
+text("The moonlight ripples across the water the way information ripples through the internet", width /2, height - 60);
 }
 
 function mouseClicked(){
-  hum.loop();
-  hum.amp(0.5);
-  video.loop(0.2);
-  video.amp(0.2);
-  if(getAudioContext().state !== 'running'){
-  getAudioContext().resume()
+ if (!sound.isPlaying()) {
+  sound.loop();
+  sound.setVolume(0.5);
+ }
 }
+
+if (getAudioContext().state !== 'running'){
+  getAudioContext().resume();
 }
