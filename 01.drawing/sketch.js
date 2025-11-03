e0dab9d1fef971a0b95d84c325c29d6297cef9de
ana-siddiqi <anasiddiqi07@outlook.com>
2025-11-03 13:17:13 +0000

    Add files via upload

--- 01.drawing/sketch.js
+++ 01.drawing/sketch.js
@@ -1,16 +1,36 @@
-function setup() {
+function setup(){
   createCanvas(400, 400);
-  background (0, 0, 200);
 }
 
-function draw() {
-  //background(220);
-  circle(width/2, 50, 50);
-  fill(255, 0, 0)
-  circle(60, mouseX, 50);
-  fill(0, 255, 0)
-  circle(70, 50, 50);
-  fill(0, 0, 255)
-  circle(80, 50, 50);
+function draw(){
+  fill(255, 0, 0);
+  background(100, 150, 255);
 
+  //face
+  fill(255, 200, 0);
+  circle(200,200,250);
+
+  //mouth
+ fill(0,0,0);
+ ellipse(200, 220, 30, 90);
+
+  //eyes
+  fill(0,0,0);
+  circle(150,150,60);
+  fill(0,0,0);
+  circle(250,150,60);
+
+  //pupils
+  fill(255);
+  circle(150,150, 20);
+  fill(255);
+  circle(250,150, 20)
+
+  //outline
+  fill(0);
+  stroke(0);
+  strokeWeight(5);
+  arc(200,220,100,60,0);
+
+  
 }
